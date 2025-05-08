91b3063c254d9ffea923783dcf83171a7614a549
coder7475 <robiulhossain7475@gmail.com>
2025-05-08 10:40:30 +0600

    feat:  a function taht filtess an items based on rating

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+// function thats updacase or lowercase given string based on conditons
 function formatString(input: string, toUpper?: boolean): string {
   if (toUpper) {
     return input.toUpperCase();
@@ -6,6 +7,35 @@ function formatString(input: string, toUpper?: boolean): string {
   }
 }
 
-console.log(formatString("Hello"));          // Output: "HELLO"
-console.log(formatString("Hello", true));   // Output: "HELLO"
-console.log(formatString("Hello", false));  // Output: "hello"
+// console.log(formatString("Hello"));          // Output: "HELLO"
+// console.log(formatString("Hello", true));   // Output: "HELLO"
+// console.log(formatString("Hello", false));  // Output: "hello"
+
+// types
+
+type Item = {
+  title: string;
+  rating: number;
+}
+// a function taht filtess an items based on rating
+function filterByRating(items: Item[]): Item[] {
+  const result = [];
+
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].rating >= 4) {
+      result.push(items[i])
+    }
+  }
+
+  return result;
+}
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 }
+];
+
+console.log(filterByRating(books));
+// Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
+
